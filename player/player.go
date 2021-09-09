254943c90e74f2d875a077f63aa9340fb252366c
Mike Vandersanden <mike.vandersanden@student.uhasselt.be>
2021-09-09 17:07:40 +0200

    only log buffer occupancy at end of loop

--- player/player.go
+++ player/player.go
@@ -541,12 +541,6 @@ func streamLoop(streamStructs []http.StreamStruct, Noden P2Pconsul.NodeUrl) (int
 	// lets loop over our mimeTypes
 	for mimeTypeIndex := range mimeTypes {
 
-		bufferStats := abrqlog.NewBufferStats()
-		bufferStats.PlayoutTime = time.Duration(playPosition) * time.Millisecond
-		bufferStats.BufferTime = time.Duration(bufferLevel) * time.Millisecond
-		bufferStats.MaxTime = time.Duration(streamStructs[mimeTypeIndex].MaxBuffer) * time.Second
-		abrqlog.MainTracer.UpdateBufferOccupancy(mimeTypesMediaType[mimeTypeIndex], bufferStats)
-
 		// get the values from the stream struct
 		segmentNumber := streamStructs[mimeTypeIndex].SegmentNumber
 		currentURL := streamStructs[mimeTypeIndex].CurrentURL
@@ -1204,6 +1198,7 @@ func streamLoop(streamStructs []http.StreamStruct, Noden P2Pconsul.NodeUrl) (int
 		}
 		streamStructs[mimeTypeIndex] = streaminfo
 
+		bufferStats := abrqlog.NewBufferStats()
 		bufferStats.PlayoutTime = time.Duration(playPosition) * time.Millisecond
 		bufferStats.BufferTime = time.Duration(bufferLevel) * time.Millisecond
 		bufferStats.MaxTime = time.Duration(streamStructs[mimeTypeIndex].MaxBuffer) * time.Second
