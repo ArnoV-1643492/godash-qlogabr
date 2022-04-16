c4e7cc59491f1de73ebce31a766fc4eca2071973
Arno Verstraete <arno.verstraete@student.uhasselt.be>
2022-04-16 12:31:22 +0200

    Adding helper functions for handling incoming events

--- crosslayer/crosslayerHelpers.go
+++ crosslayer/crosslayerHelpers.go
@@ -1,8 +1,6 @@
 package crosslayer
 
 import (
-	"fmt"
-
 	"github.com/lucas-clemente/quic-go/qlog"
 )
 
@@ -17,15 +15,36 @@ func (a CrossLayerAccountant) Listen() {
 
 func (a CrossLayerAccountant) channelListenerThread() {
 	for msg := range a.EventChannel {
-		//msgJSON, _ := gojay.Marshal(msg)
+		details := msg.GetEventDetails()
+		eventName := details.Name()
+		if eventName == "packet_received" {
+			//qlog.eventPacketReceived(details)
+		}
+
+		/*msgJSON, _ := gojay.Marshal(msg)
+		msgStr := string(msgJSON)
+		var reading map[string]interface{}
+		err := json.Unmarshal([]byte(msgStr), &reading)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			//fmt.Println(reading["name"])
+			if reading["name"] == "transport:packet_received" {
+				fmt.Println(reading["name"])
+				fmt.Println(reading["data"].(map[string]interface{})["raw"].(map[string]interface{})["length"].(string))
+			}
+		}
 		//var result map[string]interface{}
 		//gojay.Unmarshal(msgJSON, &result)
-		msgTime := msg.RelativeTime.Seconds()
+		/*msgTime := msg.RelativeTime.Seconds()
 		msgType := msg.Name()
 		if msgType == "packet_received" {
 			//msgSize := msg.eventDetails.Payload
-			fmt.Println(msgType)
-			fmt.Println(msgTime)
-		}
+			payloadlen := msg.GetEventPayloadLength()
+			if payloadlen != 0 {
+				fmt.Println(msgTime)
+				fmt.Println(payloadlen)
+			}
+		}*/
 	}
 }
